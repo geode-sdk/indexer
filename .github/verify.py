3960906381e858eb29d5ddc4fa358ffdc9ffd72f
camila314 <47485054+camila314@users.noreply.github.com>
2022-11-11 18:00:40 -0600

    a

--- .github/verify.py
+++ .github/verify.py
@@ -4,4 +4,4 @@ import json
 author = sys.argv[1]
 
 config = json.load(open("config.json", "r"))
-print("true" if author not in config["verified"] else "false")
+print("true" if author in config["verified"] else "false")
