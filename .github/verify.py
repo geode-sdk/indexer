9c9fce6a1424922b29e621f403896832102ad461
altalk23 <45172705+altalk23@users.noreply.github.com>
2023-08-07 01:38:26 +0300

    fix verify to use yes and no

--- .github/verify.py
+++ .github/verify.py
@@ -4,4 +4,4 @@ import json
 author = sys.argv[1]
 
 config = json.load(open("config.json", "r"))
-print("true" if author in config["verified"] else "false")
+print("YES" if author in config["verified"] else "NO")
